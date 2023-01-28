449e24028af98356038d978d3fbcdad94793cd49
jane <xianshenggujiu@live.com>
2023-01-28 16:53:53 +0000

    final commit

---
+++ app/src/main/java/com/jane/coject/activities/SignInActivity.kt
@@ -0,0 +1,109 @@
+package com.jane.coject.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.WindowManager
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.jane.coject.R
+import com.jane.coject.firebase.FirestoreClass
+import com.jane.coject.model.User
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignInActivity : BaseActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        btn_sign_in.setOnClickListener{
+            signInRegisteredUser()
+        }
+        setupActionBar()
+    }
+
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_sign_in_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        toolbar_sign_in_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    /**
+     * A function for Sign-In using the registered user using the email and password.
+     */
+    private fun signInRegisteredUser() {
+        // Here we get the text from editText and trim the space
+        val email: String = et_signin_email.text.toString().trim { it <= ' ' }
+        val password: String = et_signin_password.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)) {
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            // Sign-In using FirebaseAuth
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+                        // Calling the FirestoreClass signInUser function to get the data of user from database.
+//                        FirestoreClass().loadUserData(this@SignInActivity)
+                        Log.d("Sign in", "sign in with email: success")
+                        FirestoreClass().loadUserData(this@SignInActivity)
+                    } else {
+                        Log.w("Sign in", "sign in with email: failed", task.exception)
+                        Toast.makeText(
+                            this@SignInActivity,
+                            task.exception!!.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    /**
+     * A function to validate the entries of a user.
+     */
+    private fun validateForm(email: String, password: String): Boolean {
+        return if (TextUtils.isEmpty(email)) {
+            showErrorSnackBar("Please enter email.")
+            false
+        } else if (TextUtils.isEmpty(password)) {
+            showErrorSnackBar("Please enter password.")
+            false
+        } else {
+            true
+        }
+    }
+
+    /**
+     * A function to get the user details from the firestore database after authentication.
+     */
+    fun signInSuccess(user: User) {
+
+        hideProgressDialog()
+
+        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+        this.finish()
+    }
+}
---
+++ app/src/main/java/com/jane/coject/activities/SplashActivity.kt
@@ -0,0 +1,45 @@
+package com.jane.coject.activities
+
+import android.content.Intent
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.jane.coject.R
+import com.jane.coject.firebase.FirestoreClass
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val typeface: Typeface =
+            Typeface.createFromAsset(assets, "carbon bl.ttf")
+        tv_app_name.typeface = typeface
+
+        // Adding the handler to after the a task after some delay.
+        Handler().postDelayed({
+
+            // Get the current user id
+            val currentUserID = FirestoreClass().getCurrentUserID()
+            // Start the Intro Activity
+
+            if (currentUserID.isNotEmpty()) {
+                // Start the Main Activity
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            } else {
+                // Start the Intro Activity
+                startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+            }
+
+            finish() // Call this when your activity is done and should be closed.
+        }, 2500)
+    }
+}
